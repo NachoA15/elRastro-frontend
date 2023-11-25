3a0cf69bf2afe1808d3d89331f5c16653853c5d6
Nacho <100839254+NachoA15@users.noreply.github.com>
2023-11-25 18:46:22 +0100

    Página de productos

--- src/service/productoService.js
+++ src/service/productoService.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const getProductos = (setProductos) => {
     Axios.get("http://127.0.0.1:5001/producto")
     .then((res) => {
-        setProductos(res.data)
+        setProductos(res.data.productos)
     })
 }
 
