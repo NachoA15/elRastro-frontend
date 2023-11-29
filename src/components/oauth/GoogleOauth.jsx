ee57f2b7ae42b39c4e5a34d5f451d63a9f3a0a8c
Miguel-Cosano <miguecosano2001@uma.es>
2023-11-29 16:46:43 +0100

    Basic login functionality added

---
+++ src/components/oauth/GoogleOauth.jsx
@@ -0,0 +1,37 @@
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import { useEffect, useState } from "react";
+
+export default function GoogleOauth() {
+    // Obtener el token del localStorage al cargar el componente
+    const initialToken = localStorage.getItem("googleToken") || "";
+    const [token, setToken] = useState(initialToken);
+
+    // Guardar en localStorage cuando el token cambia
+    useEffect(() => {
+        localStorage.setItem("googleToken", token);
+    }, [token]);
+
+    const logUser = useGoogleLogin({
+        onSuccess: tokenResponse => {
+            const auxToken = tokenResponse.access_token;
+            setToken(auxToken);
+        },
+        onError: error => {
+            console.log(error);
+        }
+    });
+
+    const logOutUser = () => {
+        googleLogout();
+        setToken("");
+        window.location.reload();
+    };
+
+    if (token) {
+        return <button onClick={logOutUser}>Cerrar sesión</button>;
+    } else {
+        return <button onClick={logUser}>Iniciar sesión con Google</button>;
+    }
+}
+
+
