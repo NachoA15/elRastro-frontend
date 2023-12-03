ddfeb048da1fe0552f50c3c1183570b24ef7b71f
Nacho <100839254+NachoA15@users.noreply.github.com>
2023-12-03 18:52:39 +0100

    Productos/Pujas

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Productos from './components/productos/paginaProductos.jsx'
+import PaginaProducto from './components/productos/paginaProducto.jsx'
 import PaginaPrincipal from './components/principal/Principal.jsx'
 import Chats from './components/chats/paginaChats.jsx'
 import App from './App.jsx'
 import './assets/css/index.css'
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PaginaPrincipal />
+  },
   {
     path: '/productos',
     element: <Productos />
@@ -17,8 +22,10 @@ const router = createBrowserRouter([
     element: <Productos />
   },
   {
-    path: '/',
-    element: <PaginaPrincipal /> 
+    path: '/producto/:id',
+    element: <PaginaProducto />
+  },
+  {
     path: '/chats',
     element: <Chats />
   }
