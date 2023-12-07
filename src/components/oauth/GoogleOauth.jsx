da3c429863cbcde31386b8f45b61992d3ab96710
miguel cosano <miguel.cosano@babelgroup.com>
2023-12-07 17:30:42 +0100

    Added external logout

--- src/components/oauth/GoogleOauth.jsx
+++ src/components/oauth/GoogleOauth.jsx
@@ -5,8 +5,10 @@ export default function GoogleOauth() {
     // Obtener el token del localStorage al cargar el componente
     const initialToken = localStorage.getItem("googleToken") || "";
     const initialUsuario = localStorage.getItem("usuario") || "";
+    const initialLogout = localStorage.getItem("logout") || "false";
     const [token, setToken] = useState(initialToken);
     const [usuario, setUsuario] = useState(initialUsuario);
+    const [logout, setLogout] = useState(initialLogout);
 
     useEffect(() => {
         localStorage.setItem("usuario", usuario);
@@ -17,23 +19,32 @@ export default function GoogleOauth() {
         localStorage.setItem("googleToken", token);
     }, [token]);
 
+    useEffect(() => {
+        localStorage.setItem("logout", logout);
+    }, [logout]);
+
     const logUser = useGoogleLogin({
         onSuccess: tokenResponse => {
             const auxToken = tokenResponse.access_token;
             setToken(auxToken);
-
+            setLogout("false");
             usuarioService.checkToken(auxToken,logOutUser);
-
-            //Si el token se ha validado correctamente se carga el usuario en memoria
-
             usuarioService.getUsuario(auxToken, setUsuario);
-
-
         },
         onError: error => {
             console.log(error);
         }
     });
+
+    //En caso de que la sesion este iniciada va a hacer un checktoken para comprobar si el token ha caducado
+    if(token !== "" && usuario !== ""){
+        if(logout.localeCompare("true")){ // En caso de que la sesion este iniciada y logout este a true cierra sesion
+            logOutUser();
+        }
+        usuarioService.checkToken(token,logOutUser);
+    }
+
+
 
     const logOutUser = () => {
         googleLogout();
