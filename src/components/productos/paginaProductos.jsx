b9e4ca59fc090dfd88c7dcf531629837b6d3d5fa
Miguel-Cosano <miguecosano2001@uma.es>
2023-12-10 11:50:05 +0100

    Functional filter

--- src/components/productos/paginaProductos.jsx
+++ src/components/productos/paginaProductos.jsx
@@ -9,21 +9,62 @@ import GMap from '../maps/GoogleMap';
 
 export default function paginaProductos({ misProductos }) {
     const usuario = localStorage.getItem("email")
+    const filterSelected = localStorage.getItem("filter") || "Fecha_Desc";
     const [productos, setProductos] = useState([]);
     const [coordenadas, setCoordenadas] = useState([]);
-    
+    const [filtro, setFiltro] = useState(filterSelected);
+
 
     console.log(usuario)
 
-    if (misProductos) {
-        useEffect(() => {
-            productoService.getProductosByUsuario(setProductos, usuario)
-        }, [usuario])
-    } else {
-        useEffect(() => {
-            productoService.getProductos(setProductos)
-        }, [])
-    }
+    useEffect(() => {
+        if (misProductos) {
+
+            productoService.getProductosByUsuario(setProductos, usuario);
+
+        } else {
+            switch (filtro) {
+                case "Fecha_Asc":
+                    productoService.getProductos(setProductos, "Fecha_Asc");
+                    break;
+                case "Fecha_Desc":
+                    productoService.getProductos(setProductos, "Fecha_Desc");
+                    break;
+                case "Precio_Asc":
+                    productoService.getProductos(setProductos, "Precio_Asc");
+                    break;
+                case "Precio_Desc":
+                    productoService.getProductos(setProductos, "Precio_Desc");
+                    break;
+                case "Puja_Asc":
+                    productoService.getProductos(setProductos, "Puja_Asc");
+                    break;
+                case "Puja_Desc":
+                    productoService.getProductos(setProductos, "Puja_Desc");
+                    break;
+                case "Activa":
+                    productoService.getProductos(setProductos, "Activa");
+                    break;
+                case "Finalizada":
+                    productoService.getProductos(setProductos, "Finalizada");
+                    break;
+                default:
+                    productoService.getProductos(setProductos);
+                    break;
+            }
+        }
+    }, [misProductos, usuario, filtro]);
+    // Handle the change when the user selects a new value
+    const handleFilterChange = (event) => {
+        const selectedOption = event.target.value;
+        localStorage.setItem("filter", selectedOption)
+        setFiltro(selectedOption);
+    };
+
+
+    useEffect(() => {
+        localStorage.setItem("filter", filtro)
+    }, [filtro])
 
     useEffect(() => {
         productoService.getCoordenadasListByCodPostal(productos, setCoordenadas)
@@ -32,9 +73,10 @@ export default function paginaProductos({ misProductos }) {
 
     return(
         <>
-        <NavBar ubicacion={misProductos? 'Mis Productos' : "Productos"}/>
-        
         <div className="container-fluid main-div">
+            <div className="row">
+                <NavBar ubicacion={misProductos? 'Mis Productos' : "Productos"}/>
+            </div>
             <div className="row" id='titulo'>
                 <div className="col-md-12">
                     {misProductos? 
@@ -55,6 +97,21 @@ export default function paginaProductos({ misProductos }) {
                     
                     {/*<Filter anuncios={anuncios} setFiltro={setFiltro} setFiltrando={setFiltrando} filtrando={filtrando}/>*/}
                 </div>
+            </div>
+            <div className='row'>
+                    <label htmlFor="filtro" style={{paddingLeft: "3%", width: "fit-content"}}>Selecciona un filtro:</label>
+                    <select id="filtroSelect" onChange={handleFilterChange} value = {filtro} style={{width:"fit-content"}}>
+                        <option value="Fecha_Asc">Fecha Ascendente</option>
+                        <option value="Fecha_Desc" defaultChecked>Fecha Descendente</option>
+                        <option value="Precio_Asc">Precio Ascendente</option>
+                        <option value="Precio_Desc">Precio Descendente</option>
+                        <option value="Puja_Asc">Última Puja Ascendente</option>
+                        <option value="Puja_Desc">Última Puja Descendente</option>
+                        <option value="Activa">Activa</option>
+                        <option value="Finalizada">Inactiva</option>
+
+
+                    </select>
             </div>
             <div className='row' style={{width: "100%"}}>
                 <div className='col' style={{backgroundColor: "", width: "100%"}}>
