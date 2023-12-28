64ac82cf8bd7edafb142ad26dbf9ba9136112f62
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2023-12-28 12:11:36 +0100

    Filtro

--- src/components/productos/paginaProductos.jsx
+++ src/components/productos/paginaProductos.jsx
@@ -75,6 +75,9 @@ export default function paginaProductos({ misProductos }) {
         </header>
         {/*<!-- Section-->*/}
         <section class="py-5">
+            <Filter setProductos={setProductos} setFiltrando={setFiltrando} usuario={usuario} misProductos={misProductos}></Filter>
+        </section>
+        <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     {
